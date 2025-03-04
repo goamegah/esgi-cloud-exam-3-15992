280d00c0558635889117c0c9e89307beec2667a6
goamegah <komlan.godwin.amegah@gmail.com>
2025-03-04 11:08:37 +0100

    init

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    endpoint: process.env.AWS_ENDPOINT, // TODO
+    bucket: process.env.AWS_BUCKET , // TODO
   },
   suffix: {
     small: '_small.jpg',
